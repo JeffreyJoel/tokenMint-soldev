119bdaefbf4f58414380f72ed6320737fe332162
JeffreyJoel <jowoloko@gmail.com>
2024-06-03 12:19:37 +0100

    finish create-token-lab

---
+++ mint-tokens.ts
@@ -0,0 +1,36 @@
+import { mintTo } from "@solana/spl-token";
+import "dotenv/config";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+const user = getKeypairFromEnvironment("SECRET_KEY");
+
+// Subtitute in your token mint account from create-token-mint.ts
+const tokenMintAccount = new PublicKey(
+    "CSXRmwLkDuqHCeYySaUpXyJ9K5LPc5AqdEAu7kuu3M1K"
+);
+
+// Substitute in your own, or a friend's token account address, based on the previous step.
+const recipientAssociatedTokenAccount = new PublicKey(
+  "4Y2UcKWNdzGWLqgCupD79Yea2CLEKm6DwprLumsL3oWb"
+);
+
+const transactionSignature = await mintTo(
+  connection,
+  user,
+  tokenMintAccount,
+  recipientAssociatedTokenAccount,
+  user,
+  10 * MINOR_UNITS_PER_MAJOR_UNITS
+);
+
+const link = getExplorerLink("transaction", transactionSignature, "devnet");
+
+console.log(`✅ Success! Mint Token Transaction: ${link}`);
---
+++ transfer-tokens.ts
@@ -0,0 +1,54 @@
+import "dotenv/config";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `🔑 Loaded our keypair securely, using an env file! Our public key is: ${sender.publicKey.toBase58()}`
+);
+
+// Add the recipient public key here.
+const recipient = new PublicKey("D7WnKKt2qxSTyZ2Q6uiPFoZVu2oBbZ4XqYcAgWEaAzFF");
+
+// Subtitute in your token mint account
+const tokenMintAccount = new PublicKey("CSXRmwLkDuqHCeYySaUpXyJ9K5LPc5AqdEAu7kuu3M1K");
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(1, 2);
+
+console.log(`💸 Attempting to send 0.1 token to ${recipient.toBase58()}...`);
+
+// Get or create the source and destination token accounts to store this token
+const sourceTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  sender.publicKey
+);
+
+const destinationTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  recipient
+);
+
+// Transfer the tokens
+const signature = await transfer(
+  connection,
+  sender,
+  sourceTokenAccount.address,
+  destinationTokenAccount.address,
+  sender,
+  1 * MINOR_UNITS_PER_MAJOR_UNITS
+);
+
+const explorerLink = getExplorerLink("transaction", signature, "devnet");
+
+console.log(`✅ Transaction confirmed, explorer link is: ${explorerLink}!`);
